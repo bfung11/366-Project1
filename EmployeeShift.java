8c4a7300ebd5f5986f4fb53b7040c7be0c93592f
Brian Fung <bfung11@gmail.com>
2016-04-18 16:12:25 -0700

    Updating all dates to use Calendar

--- EmployeeShift.java
+++ EmployeeShift.java
@@ -1,5 +1,5 @@
 public class EmployeeShift {
    private int employeeID;
-   private String date; //TODO may need to change datatypes
+   private Calendar date; //TODO may need to change datatypes
    private int shiftID;
 }
--- EmployeeTimeOff.java
+++ EmployeeTimeOff.java
@@ -1,7 +1,5 @@
 public class EmployeeTimeOff {
    private int employeeID;
-   private String fromDate;
-   private String fromTime;
-   private String toDate;
-   private String toTime;
+   private Calendar fromDate;
+   private Calendar toDate;
 }
