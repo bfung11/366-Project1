1d95cdcce2f632a13286e6f805f2d52f2b70988d
Brian Fung <bfung11@gmail.com>
2016-04-18 11:36:39 -0700

    Adding fields to Shift to match database table

--- Shift.java
+++ Shift.java
@@ -1,9 +1,6 @@
 public class Shift {
-   // time to;
-   // time from;
-   private int doctorID_1;
-   private int doctorID_2;
-   private int technician_1;
-   private int technician_2;
-   private int shiftID;
+   private int id;
+   private String name;
+   private String fromTime; //TODO may need to change datatypes
+   private String toTime; //TODO may need to change datatypes
 }
