2e5c43c80ee23f6a415478e4818ae75f3e334b60
Brian Fung <bfung11@gmail.com>
2016-04-29 23:23:32 -0700

    Styling ReadMe

--- Employee.java
+++ Employee.java
@@ -1,8 +1,40 @@
 public class Employee {
-   private int id;
+   DBConnection connection;
+
+   private Integer id;
    private String email;
    private String password;
    private String firstname;
    private String lastname;
    private String phone;
+
+   public Employee(String type) {
+      connection = new DBConnection();
+
+      execQuery("SELECT * ")  
+   }
+
+   public Integer getID() {
+      return id;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public String getFirstName() {
+      return firstname;
+   }
+
+   public String getLastName() {
+      return lastname;
+   }
+
+   public String getPhoneNumber() {
+      return phone;
+   }
 }
