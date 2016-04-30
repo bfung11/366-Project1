b19f543d16699181de5ab3bf1656c90f66daedca
Brian Fung <bfung11@gmail.com>
2016-04-30 12:56:49 -0700

    Adding all required classes back into the java folder

---
+++ src/java/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+   DBConnection connection;
+
+   private Integer id;
+   private String email;
+   private String password;
+   private String firstname;
+   private String lastname;
+   private String phone;
+
+   public Employee(String type) {
+      connection = new DBConnection();
+   }
+
+   public Integer getID() {
+      return id;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public String getFirstName() {
+      return firstname;
+   }
+
+   public String getLastName() {
+      return lastname;
+   }
+
+   public String getPhoneNumber() {
+      return phone;
+   }
+}
