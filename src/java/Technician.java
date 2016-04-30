1917df10cba3feb10d9f1a18a7f7cd21e082c996
Brian Fung <bfung11@gmail.com>
2016-04-30 15:58:43 -0700

    Adding technician.java

---
+++ src/java/Technician.java
@@ -0,0 +1,29 @@
+public class Technician extends Employee {
+   public Technician() {
+      super("technician");
+   }
+
+   public Integer getID() {
+      return super.getID();
+   }
+
+   public String getEmail() {
+      return super.getEmail();
+   }
+
+   public String getPassword() {
+      return super.getPassword();
+   }
+
+   public String getFirstName() {
+      return super.getFirstName();
+   }
+
+   public String getLastName() {
+      return super.getLastName();
+   }
+
+   public String getPhoneNumber() {
+      return super.getPhoneNumber();
+   }
+}
