a366af06a6fefee145c0581036ff1c17dbacd7b1
Brian Fung <bfung11@gmail.com>
2016-05-01 12:00:14 -0700

    Adding Admin class

---
+++ src/java/Admin.java
@@ -0,0 +1,33 @@
+public class Admin extends Employee {
+   public Admin() {
+      super(Employee.ADMINISTRATOR);
+   }
+
+   public int getID() {
+      return super.getID();
+   }
+
+   public String getEmail() {
+      return super.getEmail();
+   }
+
+   public String getPassword() {
+      return super.getPassword();
+   }
+
+   public String getFirstName() {
+      return super.getFirstName();
+   }
+
+   public String getLastName() {
+      return super.getLastName();
+   }
+
+   public String getPhoneNumber() {
+      return super.getPhoneNumber();
+   }
+
+   public int getTimeOff() {
+      return super.getTimeOff();
+   }
+}
