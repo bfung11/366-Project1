d8d4c99d7c68e5c28a9d06820417cbbaff80f324
Brian Fung <bfung11@gmail.com>
2016-05-01 22:30:26 -0700

    Fixing table entries and changing phone to phonenumber

--- src/java/CreateData.sql
+++ src/java/CreateData.sql
@@ -6,24 +6,24 @@ VALUES ('7:30 Shift', '07:30', '18:30'),
        ('Overnight Shift', '20:00', '08:30'),
        ('Sunday Shift', '08:00', '20:00');
 
-INSERT INTO Doctors (email, password, lastname, phone)
-VALUES ('first@gmail.com', 'Doctor', 'First', '(111) 111-1111'),
-       ('second@gmail.com', 'Doctor', 'Second', '(111) 111-1111'),
-       ('third@gmail.com', 'Doctor', 'Third', '(111) 111-1111'),
-       ('fourth@gmail.com', 'Doctor', 'Fourth', '(111) 111-1111'),
-       ('fifth@gmail.com', 'Doctor', 'Fifth', '(111) 111-1111'),
-       ('sixth@gmail.com', 'Doctor', 'Sixth', '(111) 111-1111'),
-       ('seventh@gmail.com', 'Doctor', 'Seventh', '(111) 111-1111'),
-       ('eighth@gmail.com', 'Doctor', 'Eighth', '(111) 111-1111'),
-       ('ninth@gmail.com', 'Doctor', 'Ninth', '(111) 111-1111');
+INSERT INTO Doctors (email, username, password, lastname, phonenumber)
+VALUES ('first@gmail.com', 'first', 'password', 'First', '(111) 111-1111'),
+       ('second@gmail.com', 'second', 'password', 'Second', '(111) 111-1111'),
+       ('third@gmail.com', 'third', 'password', 'Third', '(111) 111-1111'),
+       ('fourth@gmail.com', 'fourth', 'password', 'Fourth', '(111) 111-1111'),
+       ('fifth@gmail.com', 'fifth', 'password', 'Fifth', '(111) 111-1111'),
+       ('sixth@gmail.com', 'sixth', 'password', 'Sixth', '(111) 111-1111'),
+       ('seventh@gmail.com', 'seventh', 'password', 'Seventh', '(111) 111-1111'),
+       ('eighth@gmail.com', 'eighth', 'password', 'Eighth', '(111) 111-1111'),
+       ('ninth@gmail.com', 'ninth', 'password', 'Ninth', '(111) 111-1111');
 
-INSERT INTO Technicians (email, password, lastname, phone)
-VALUES ('first@yahoo.com', 'Technician', 'First', '(111) 111-1111'),
-       ('second@yahoo.com', 'Technician', 'Second', '(111) 111-1111'),
-       ('third@yahoo.com', 'Technician', 'Third', '(111) 111-1111'),
-       ('fourth@yahoo.com', 'Technician', 'Fourth', '(111) 111-1111'),
-       ('fifth@yahoo.com', 'Technician', 'Fifth', '(111) 111-1111'),
-       ('sixth@yahoo.com', 'Technician', 'Sixth', '(111) 111-1111'),
-       ('seventh@yahoo.com', 'Technician', 'Seventh', '(111) 111-1111'),
-       ('eighth@yahoo.com', 'Technician', 'Eighth', '(111) 111-1111'),
-       ('ninth@yahoo.com', 'Technician', 'Ninth', '(111) 111-1111');
+INSERT INTO Technicians (email, username, password, lastname, phonenumber)
+VALUES ('first@yahoo.com', 'first', 'password', 'First', '(111) 111-1111'),
+       ('second@yahoo.com', 'second', 'password', 'Second', '(111) 111-1111'),
+       ('third@yahoo.com', 'third', 'password', 'Third', '(111) 111-1111'),
+       ('fourth@yahoo.com', 'fourth', 'password', 'Fourth', '(111) 111-1111'),
+       ('fifth@yahoo.com', 'fifth', 'password', 'Fifth', '(111) 111-1111'),
+       ('sixth@yahoo.com', 'sixth', 'password', 'Sixth', '(111) 111-1111'),
+       ('seventh@yahoo.com', 'seventh', 'password', 'Seventh', '(111) 111-1111'),
+       ('eighth@yahoo.com', 'eighth', 'password', 'Eighth', '(111) 111-1111'),
+       ('ninth@yahoo.com', 'ninth', 'password', 'Ninth', '(111) 111-1111');
