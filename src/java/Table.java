a9d9b1457a19a0ff1de1e37258fe063a8100cd89
Brian Fung <bfung11@gmail.com>
2016-05-04 21:44:13 -0700

    Adding static getTableName() and getType()

--- src/java/Table.java
+++ src/java/Table.java
@@ -10,4 +10,55 @@ public class Table {
    public final static String SICK_DAYS = "sickDaysLeft";
    public final static String DATE = "date";
    public final static String SHIFT_ID = "shift";
+
+   private static int employeeType;
+
+   public static String getTableName(String secondPart) {
+      String tablename = "";
+
+      switch(employeeType) {
+         case Employee.DOCTOR:
+            tablename = "Doctor" + secondPart;
+            break;
+         case Employee.TECHNICIAN:
+            tablename = "Technician" + secondPart;
+            break;
+         case Employee.ADMINISTRATOR:
+            tablename = "Administrator" + secondPart;
+            break;
+         default:
+            break;
+      }
+
+      return tablename;
+   }
+
+   public static String getTableName(int requestType) {
+      String tablename = "";
+
+      switch (requestType) {
+         case Request.SICK_DAY:
+            tablename = Table.getTableName("SickDays");
+            break;
+         case Request.VACATION_DAY: 
+            tablename = Table.getTableName("VacationDays");
+            break;
+         case Request.PREFERRED_SHIFT: 
+            tablename = Table.getTableName("PreferredShifts");
+            break;
+         default:
+            System.out.println("Not a valid employeeType");
+            break;
+      }
+
+      return tablename;
+   }
+
+   public static void setEmployeeType(int employeeType) {
+      this.employeeType = employeeType;
+   }
+
+   public static int getEmployeeType() {
+      return employeeType;
+   }
 }
