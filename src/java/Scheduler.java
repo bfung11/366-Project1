423a6f77ffaed541d65b507a126044cb6d30ff48
jzam <jzaman03@gmail.com>
2016-05-04 22:52:43 -0700

    Added request constraint checking for scheduler

--- src/java/Scheduler.java
+++ src/java/Scheduler.java
@@ -18,6 +18,9 @@ public class Scheduler {
    private final static int MAX_SHIFTS_PER_WEEK = 4;
    private final static int INVALID = -1;
    private final static int TOTAL_SHIFTS_PER_WEEK = 32;
+   private final static int TYPICAL_SHIFTS_PER_DAY = 5;
+   
+   //Indexing constants
    private final static int SUNDAY = 0;
    private final static int MONDAY = 1;
    private final static int TUESDAY = 2;
@@ -25,6 +28,14 @@ public class Scheduler {
    private final static int THURSDAY = 4;
    private final static int FRIDAY = 5;
    private final static int SATURDAY = 6;
+   
+   private final static int EarlyMorning = 0;
+   private final static int Morning = 1;
+   private final static int LateMorning = 2;
+   private final static int Surgery = 3;
+   private final static int Overnight = 4;
+   private final static int SundayMorning = 31;
+   private final static int SundayOvernight = 30;
    
    private int[] SundayIndices;
    private int[] MondayIndices;
@@ -60,7 +71,7 @@ public class Scheduler {
    }
    
    //Associate the 32 shift indices with special attributes like day, overnight,
-   //or surgery
+   //or surgery AKA "Magic"
    private void initDayIndices() {
       SundayIndices = new int[] {30, 31};
       MondayIndices = new int[] {0, 1, 2, 3, 4};
@@ -70,7 +81,7 @@ public class Scheduler {
       FridayIndices = new int[] {20, 21, 22, 23, 24};
       SaturdayIndices = new int[] {25, 26, 27, 28, 29};
       
-      OvernightIndices = new int[] {4, 9, 14, 19, 24};
+      OvernightIndices = new int[] {4, 9, 14, 19, 24, 29, 30};
       SurgeryIndices = new int[] {3, 8, 13, 18, 23};
    }
 
@@ -196,7 +207,97 @@ public class Scheduler {
    }
    
    private boolean checkGoodRequest(int[] schedules, Request request) {
+      int docID;
+      Calendar date;
+      String shiftName;
       
+      docID = request.getDoctorID();
+      date = request.getDate();
+      
+      if (request.getType() == Request.PREFERRED_SHIFT) {
+         shiftName = request.getShiftName();
+         return checkPreferredShift(schedules, docID, date, shiftName);
+      }
+      else {
+         return checkTimeOff(schedules, docID, date);
+      }
+   }
+   
+   private boolean checkPreferredShift(int[] schedules, int docID, Calendar date, String shiftName) {
+      int dayOfWeek;
+      int[] dayIndices;
+      int shiftOffset;
+      
+      dayOfWeek = date.get(Calendar.DAY_OF_WEEK);
+      
+      switch(shiftName) {
+         case Shift.EARLY:
+            shiftOffset = EarlyMorning;
+            break;
+         case Shift.MORNING:
+            shiftOffset = Morning;
+            break;
+         case Shift.LATE:
+            shiftOffset = LateMorning;
+            break;
+         case Shift.SURGERY:
+            shiftOffset = Surgery;
+            break;
+         case Shift.OVERNIGHT:
+            shiftOffset = Overnight;
+            if (dayOfWeek == Calendar.SUNDAY)
+               shiftOffset = SundayOvernight;
+            break;
+         case Shift.SUNDAY:
+            shiftOffset = SundayMorning;
+            break;
+         default:
+            System.out.println("Got a weird shift name: " + shiftName);
+            return false;    
+      }
+            
+      if (shiftOffset != SundayOvernight && shiftOffset != SundayMorning) {
+         shiftOffset = dayOfWeek * TYPICAL_SHIFTS_PER_DAY + shiftOffset;
+      }
+      
+      return (schedules[shiftOffset] == docID); 
+   }
+   
+   private boolean checkTimeOff(int[] schedules, int docID, Calendar date) {
+      int dayOfWeek;
+      int[] dayIndices;
+      int i;
+      
+      dayOfWeek = date.get(Calendar.DAY_OF_WEEK);
+      
+      switch(dayOfWeek) {
+         case Calendar.SUNDAY:
+            dayIndices = SundayIndices;
+            break;
+         case Calendar.MONDAY:
+            dayIndices = MondayIndices;
+            break;
+         case Calendar.TUESDAY:
+            dayIndices = TuesdayIndices;
+            break;
+         case Calendar.WEDNESDAY:
+            dayIndices = WednesdayIndices;
+            break;
+         case Calendar.THURSDAY:
+            dayIndices = ThursdayIndices;
+            break;
+         case Calendar.FRIDAY:
+            dayIndices = FridayIndices;
+            break;
+         default:
+            dayIndices = SaturdayIndices;
+            break;
+      }
+      
+      for (i = 0; i < dayIndices.length; i++) {
+         if (dayIndices[i] == docID)
+            return true;
+      }
       return false;
    }
 
